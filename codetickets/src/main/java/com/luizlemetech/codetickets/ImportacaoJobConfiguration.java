74e21b9134173501197c8ac6ca2faf3672a9b3eb
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-21 09:42:32 -0300

    feat: implementado a classe de configuracao do job ImportacaoJobConfiguration

---
+++ codetickets/src/main/java/com/luizlemetech/codetickets/ImportacaoJobConfiguration.java
@@ -0,0 +1,71 @@
+package com.luizlemetech.codetickets;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ImportacaoJobConfiguration {
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job job(Step passoInicial, JobRepository jobRepository) {
+        return new JobBuilder("geracao-tickets", jobRepository)
+                .start(passoInicial)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step passoInicial(ItemReader<Importacao> reader, ItemWriter<Importacao> writer, JobRepository jobRepository) {
+        return new StepBuilder("passo-inicial", jobRepository)
+                .<Importacao, Importacao>chunk(200, transactionManager)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Importacao> reader() {
+        return new FlatFileItemReaderBuilder<Importacao>()
+                .name("leitura-csv")
+                .resource(new FileSystemResource("files/dados.csv"))
+                .comments("--")
+                .delimited()
+                .delimiter(";")
+                .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
+                .fieldSetMapper(new ImportacaoMapper())
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Importacao> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Importacao>()
+                .dataSource(dataSource)
+                .sql(
+                        "INSERT INTO importacao (cpf, cliente, nascimento, evento, data, tipo_ingresso, valor, hora_importacao) VALUES" +
+                                " (:cpf, :cliente, :nascimento, :evento, :data, :tipoIngresso, :valor, :horaImportacao)"
+
+                )
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .build();
+    }
+}
+
