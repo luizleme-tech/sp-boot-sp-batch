5f4e39bbf8d1dd70e9b2a48d0b178990efd148f6
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-21 09:43:31 -0300

    chore: arquivos de importacao

---
+++ codetickets/src/test/java/com/luizlemetech/codetickets/CodeticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizlemetech.codetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
