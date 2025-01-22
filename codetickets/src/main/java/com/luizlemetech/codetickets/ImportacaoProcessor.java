504ddcd0eab95a4fe8ecc7c8ab4be72bd4f656b8
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-01-21 21:17:10 -0300

    feat: implementado um itemProcessor que seta a taxa administrativa

---
+++ codetickets/src/main/java/com/luizlemetech/codetickets/ImportacaoProcessor.java
@@ -0,0 +1,17 @@
+package com.luizlemetech.codetickets;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class ImportacaoProcessor implements ItemProcessor<Importacao, Importacao> {
+    @Override
+    public Importacao process(Importacao item) throws Exception {
+        if (item.getTipoIngresso().equalsIgnoreCase("vip")) {
+            item.setTaxaAdm(130.0);
+        } else if (item.getTipoIngresso().equalsIgnoreCase("camarote")) {
+            item.setTaxaAdm(80.0);
+        } else {
+            item.setTaxaAdm(50.0);
+        }
+        return item;
+    }
+}
